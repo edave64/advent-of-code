428c826cf0108b0fa085ce15536b8f1220447679
edave64 <github5815@xcvbnm.org>
2024-12-04 20:37:17 +0100

    Missing prettier for day 3

--- 2024/03/part01/solution.ts
+++ 2024/03/part01/solution.ts
@@ -2,11 +2,11 @@ import { promises as fs } from "fs";
 
 let file: fs.FileHandle;
 try {
-    file = await fs.open(import.meta.dirname + "/../input.txt", "r");
+	file = await fs.open(import.meta.dirname + "/../input.txt", "r");
 } catch (e) {
-    console.error("No input file found");
-    console.error(e);
-    process.exit(1);
+	console.error("No input file found");
+	console.error(e);
+	process.exit(1);
 }
 
 const fulltext = await file.readFile({ encoding: "utf8" });
@@ -14,7 +14,7 @@ const fulltext = await file.readFile({ encoding: "utf8" });
 let sum = 0;
 
 for (const [f, a, b] of fulltext.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)) {
-    sum += parseInt(a) * parseInt(b);
+	sum += parseInt(a) * parseInt(b);
 }
 
 console.log(sum);
--- 2024/03/part02/solution.ts
+++ 2024/03/part02/solution.ts
@@ -2,11 +2,11 @@ import { promises as fs } from "fs";
 
 let file: fs.FileHandle;
 try {
-    file = await fs.open(import.meta.dirname + "/../input.txt", "r");
+	file = await fs.open(import.meta.dirname + "/../input.txt", "r");
 } catch (e) {
-    console.error("No input file found");
-    console.error(e);
-    process.exit(1);
+	console.error("No input file found");
+	console.error(e);
+	process.exit(1);
 }
 
 const fulltext = await file.readFile({ encoding: "utf8" });
@@ -15,13 +15,13 @@ let sum = 0;
 let enabled = true;
 
 for (const [f, a, b] of fulltext.matchAll(/mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g)) {
-    if (f === "do()") {
-        enabled = true;
-    } else if (f === "don't()") {
-        enabled = false;
-    } else if (enabled) {
-        sum += parseInt(a) * parseInt(b);
-    }
+	if (f === "do()") {
+		enabled = true;
+	} else if (f === "don't()") {
+		enabled = false;
+	} else if (enabled) {
+		sum += parseInt(a) * parseInt(b);
+	}
 }
 
 console.log(sum);
