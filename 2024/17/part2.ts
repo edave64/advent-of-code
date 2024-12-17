3d5a5e80f2c2a1dc29aa6260e61eadc546859d4c
edave64 <github5815@xcvbnm.org>
2024-12-18 00:01:55 +0100

    Day 17: Solved part 2

--- 2024/17/part2.ts
+++ 2024/17/part2.ts
@@ -17,43 +17,54 @@ This tells us:
 - B and C depend on A
 - A has to be 0 at the end of the program
 
-The output we are trying to archive is 2,4,1,3,7,5,0,3,4,3,1,5,5,5,3,0,
+The output we are trying to archive is 2,4,1,3,7,5,0,3,4,3,1,5,5,5,3,0
 so we should walk backwards, from the last value to the first
  */
 
-// NOT FUNCTIONAL
+import { compile } from "./compiler";
+import { readProgram } from "./shared";
+
+const { b, c, program } = await readProgram(import.meta.dirname + "/input.txt");
+
+const compiled = function* anonymous(a: bigint): Generator<number> {
+	let b = 0n;
+	let c = 0n;
+	while (true) {
+		b = a & 7n;
+		b = b ^ 3n;
+		c = a >> b;
+		a = a >> 3n;
+		b = b ^ c;
+		b = b ^ 5n;
+		yield Number(b & 7n);
+		if (a === 0n) {
+			break;
+		}
+	}
+};
 
 const values = [2, 4, 1, 3, 7, 5, 0, 3, 4, 3, 1, 5, 5, 5, 3, 0];
-let iters = 0;
-function resolve(values: number[], idx: number, aReq: number, aMask: number): bigint | null {
-	iters++;
-	if (idx < 0) return 0n;
-	let b = values[idx];
-	idx++;
-	b = b ^ 0b101;
-	const localAMask = aMask & 0b111;
-	const localAReq = aReq & localAMask;
-	aMask >>= 3;
-	aReq >>= 3;
-	for (let c = 0; c <= 0b111; c++) {
-		b = b ^ c;
-		c = c << b;
-		const cMask = 0b111 << b;
-		b = b ^ 0b011;
 
-		if (localAReq !== (b & localAMask)) {
-			continue;
+let depth = values.length - 1;
+function solve(i: number, a: bigint): bigint | null {
+	if (i < 0) {
+		// Verify
+		if (Array.from(compiled(a!)).join(",") !== program) {
+			return null;
 		}
-		const nextAMask = aMask | cMask;
-		const nextAReq = aReq | c;
-
-		const a = resolve(values, idx, nextAReq, nextAMask);
-		if (a !== null) {
-			return (a << 3n) | BigInt(b);
+		return a;
+	}
+	depth = Math.min(depth, i);
+	for (let j = 0; j <= 0b1111; j++) {
+		const newA = (a << 3n) | BigInt(j);
+		const firstVal = compiled(newA).next().value;
+		if (firstVal === values[i]) {
+			const req = solve(i - 1, newA);
+			if (req !== null) return req;
 		}
 	}
 	return null;
 }
 
-console.log(resolve(values, 0, 0, 0));
-console.log(iters);
+const a = solve(values.length - 1, 0n);
+console.log(a);
