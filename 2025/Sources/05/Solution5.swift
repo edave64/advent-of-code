bb37871d089c755c136286f778518bf4a423804f
edave64 <github5815@xcvbnm.org>
2025-12-06 11:27:57 +0100

    Day 5 cleanup

--- 2025/Sources/05/Solution5.swift
+++ 2025/Sources/05/Solution5.swift
@@ -31,38 +31,6 @@ struct Solution5: Solution {
     return String(sum)
   }
 
-  func prepareRangesForBinarySearch(_ ranges: [Range<Int>]) -> [Range<Int>] {
-    var ranges = ranges
-    ranges.sort { $0.lowerBound < $1.lowerBound }
-
-    // Remove fully overlapping ranges (Those mess up binary partitioning)
-    var i = 1
-    while i < ranges.count {
-      let prevRange = ranges[i - 1]
-      let range = ranges[i]
-      if prevRange.lowerBound <= range.lowerBound && prevRange.upperBound >= range.upperBound {
-        ranges.remove(at: i)
-      } else {
-        i += 1
-      }
-    }
-    return ranges
-  }
-
-  func binarySearch(ranges: ArraySlice<Range<Int>>, num: Int) -> Bool {
-    if ranges.isEmpty {
-      return false
-    }
-    let mid = ranges.startIndex + ranges.count / 2
-    if ranges[mid].contains(num) {
-      return true
-    } else if ranges[mid].lowerBound > num {
-      return binarySearch(ranges: ranges[ranges.startIndex..<mid], num: num)
-    } else {
-      return binarySearch(ranges: ranges[(mid + 1)..<ranges.endIndex], num: num)
-    }
-  }
-
   func partB(input: String) throws -> String {
     var sum = 0
     var ranges: [Range<Int>] = []
@@ -97,47 +65,35 @@ struct Solution5: Solution {
     return String(sum)
   }
 
-  func incMap(map: [[Int]], x: Int, y: Int, val: Int = 1) -> [[Int]] {
-    var map = map
-    map[x - 1][y - 1] += val
-    map[x - 1][y] += val
-    map[x - 1][y + 1] += val
-    map[x][y - 1] += val
-    map[x][y] += val
-    map[x][y + 1] += val
-    map[x + 1][y - 1] += val
-    map[x + 1][y] += val
-    map[x + 1][y + 1] += val
-    return map
+  func prepareRangesForBinarySearch(_ ranges: [Range<Int>]) -> [Range<Int>] {
+    var ranges = ranges
+    ranges.sort { $0.lowerBound < $1.lowerBound }
+
+    // Remove fully overlapping ranges (Those mess up binary partitioning)
+    var i = 1
+    while i < ranges.count {
+      let prevRange = ranges[i - 1]
+      let range = ranges[i]
+      if prevRange.lowerBound <= range.lowerBound && prevRange.upperBound >= range.upperBound {
+        ranges.remove(at: i)
+      } else {
+        i += 1
+      }
+    }
+    return ranges
   }
 
-  func getMap(mapStr: String) -> [[Int]] {
-    let lines = mapStr.split(separator: "\n")
-    // Add padding on all sides, so we can skip bounds checking
-    var map = Array(
-      repeating: Array(repeating: 0, count: lines.first!.count + 2), count: lines.count + 2)
-
-    for (unpaddedY, line) in lines.enumerated() {
-      for (unpaddedX, char) in line.enumerated() {
-        switch char {
-        case "@":
-          map = incMap(map: map, x: unpaddedX + 1, y: unpaddedY + 1)
-        case ".":
-          break
-        default:
-          fatalError("Invalid char \(char)")
-        }
-      }
+  func binarySearch(ranges: ArraySlice<Range<Int>>, num: Int) -> Bool {
+    if ranges.isEmpty {
+      return false
     }
-
-    for (unpaddedY, line) in lines.enumerated() {
-      for (unpaddedX, char) in line.enumerated() {
-        if char == "." {
-          map[unpaddedX + 1][unpaddedY + 1] = 0
-        }
-      }
+    let mid = ranges.startIndex + ranges.count / 2
+    if ranges[mid].contains(num) {
+      return true
+    } else if ranges[mid].lowerBound > num {
+      return binarySearch(ranges: ranges[ranges.startIndex..<mid], num: num)
+    } else {
+      return binarySearch(ranges: ranges[(mid + 1)..<ranges.endIndex], num: num)
     }
-
-    return map
   }
 }
