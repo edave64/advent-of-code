577c4df7ef7f3e1717c3b696bb756dd576e5a1b3
edave64 <github5815@xcvbnm.org>
2025-12-08 21:44:09 +0100

    Day 8 optimized

--- 2025/Sources/08/Solution8.swift
+++ 2025/Sources/08/Solution8.swift
@@ -18,65 +18,43 @@ struct Solution8: Solution {
       return Point(x: x, y: y, z: z)
     })
 
-    // Store the distances between each point and every other point
-    // We are using a dictionary, since we really care about the distance value and want to sort
-    // by it, but after that we also need the points.
-    // And it's an array since two points can have the same distance from each other
-    var distances = [Int: [(Int, Int)]]()
-    for x in 0..<points.count {
-      let pointX = points[x]
-      for y in (x + 1)..<points.count {
-        let pointY = points[y]
-        let dist = pointX.distance(to: pointY)
-        if distances[dist] == nil {
-          distances[dist] = []
-        }
-        distances[dist]!.append((x, y))
-      }
-    }
-
     var pointsConnected = 0
     var clusterSizes = [Int]()
     var clusterPerPoint = Array(repeating: -1, count: points.count)
     // Shortest distances first
-    let sortedDistances = distances.sorted(by: { $0.key < $1.key })
+    let pairsByDistance = try getPairsByDistance(points: points)
 
     // Connect the junction boxes and manage the clusters
-    for (_, pointPairs) in sortedDistances {
-      for pointPair in pointPairs {
-        let pointA = pointPair.0
-        let pointB = pointPair.1
-        let clusterA = clusterPerPoint[pointA]
-        let clusterB = clusterPerPoint[pointB]
-        pointsConnected += 1
-        if clusterA == -1 && clusterB == -1 {
-          // Neither point is in a cluster yet
-          // Make a new one with 2 points
-          clusterSizes.append(2)
-          clusterPerPoint[pointA] = clusterSizes.count - 1
-          clusterPerPoint[pointB] = clusterSizes.count - 1
-        } else if clusterA == -1 {
-          // Point A is not in a cluster yet, put it in the cluster of point B
-          clusterPerPoint[pointA] = clusterB
-          clusterSizes[clusterB] += 1
-        } else if clusterB == -1 {
-          // Point B is not in a cluster yet, put it in the cluster of point A
-          clusterPerPoint[pointB] = clusterA
-          clusterSizes[clusterA] += 1
-        } else if clusterA != clusterB {
-          // Both A and B are in different clusters, merge them
-          for i in 0..<clusterPerPoint.count {
-            // Move all points from cluster B to cluster A
-            if clusterPerPoint[i] == clusterB {
-              clusterPerPoint[i] = clusterA
-            }
+    for pointPair in pairsByDistance {
+      let pointA = pointPair.0
+      let pointB = pointPair.1
+      let clusterA = clusterPerPoint[pointA]
+      let clusterB = clusterPerPoint[pointB]
+      pointsConnected += 1
+      if clusterA == -1 && clusterB == -1 {
+        // Neither point is in a cluster yet
+        // Make a new one with 2 points
+        clusterSizes.append(2)
+        clusterPerPoint[pointA] = clusterSizes.count - 1
+        clusterPerPoint[pointB] = clusterSizes.count - 1
+      } else if clusterA == -1 {
+        // Point A is not in a cluster yet, put it in the cluster of point B
+        clusterPerPoint[pointA] = clusterB
+        clusterSizes[clusterB] += 1
+      } else if clusterB == -1 {
+        // Point B is not in a cluster yet, put it in the cluster of point A
+        clusterPerPoint[pointB] = clusterA
+        clusterSizes[clusterA] += 1
+      } else if clusterA != clusterB {
+        // Both A and B are in different clusters, merge them
+        for i in 0..<clusterPerPoint.count {
+          // Move all points from cluster B to cluster A
+          if clusterPerPoint[i] == clusterB {
+            clusterPerPoint[i] = clusterA
           }
-          clusterSizes[clusterA] += clusterSizes[clusterB]
-          clusterSizes[clusterB] = 0
         }
-        if pointsConnected >= n {
-          break
-        }
+        clusterSizes[clusterA] += clusterSizes[clusterB]
+        clusterSizes[clusterB] = 0
       }
       if pointsConnected >= n {
         break
@@ -101,70 +79,51 @@ struct Solution8: Solution {
       return Point(x: x, y: y, z: z)
     })
 
-    // Calculate the distances between all points
-    var distances = [Int: [(Int, Int)]]()
-    for x in 0..<points.count {
-      let pointX = points[x]
-      for y in (x + 1)..<points.count {
-        let pointY = points[y]
-        let dist = pointX.distance(to: pointY)
-        if distances[dist] == nil {
-          distances[dist] = []
-        }
-        distances[dist]!.append((x, y))
-      }
-    }
+    // Shortest distances first
+    let pairsByDistance = try getPairsByDistance(points: points)
 
     var clusterSizes = [Int]()
     var clusterPerPoint = Array(repeating: -1, count: points.count)
-    // Shortest distances first
-    let sortedDistances = distances.sorted(by: { $0.key < $1.key })
-
     var lastPointPair: (Int, Int)?
 
     // Connect the junction boxes and manage the clusters
-    for (_, pointPairs) in sortedDistances {
-      for pointPair in pointPairs {
-        lastPointPair = pointPair
-        let pointA = pointPair.0
-        let pointB = pointPair.1
-        let clusterA = clusterPerPoint[pointA]
-        let clusterB = clusterPerPoint[pointB]
-        if clusterA == -1 && clusterB == -1 {
-          // Neither point is in a cluster yet
-          // Make a new one with 2 points
-          clusterSizes.append(2)
-          clusterPerPoint[pointA] = clusterSizes.count - 1
-          clusterPerPoint[pointB] = clusterSizes.count - 1
-        } else if clusterA == -1 {
-          // Point A is not in a cluster yet, put it in the cluster of point B
-          clusterPerPoint[pointA] = clusterB
-          clusterSizes[clusterB] += 1
-        } else if clusterB == -1 {
-          // Point B is not in a cluster yet, put it in the cluster of point A
-          clusterPerPoint[pointB] = clusterA
-          clusterSizes[clusterA] += 1
-        } else if clusterA != clusterB {
-          // Both A and B are in different clusters, merge them
-          // The one with the higher index gets removed
-          // This is so that all points eventually converge in cluster 0
-          let lower = min(clusterA, clusterB)
-          let higher = max(clusterA, clusterB)
+    for pointPair in pairsByDistance {
+      lastPointPair = pointPair
+      let pointA = pointPair.0
+      let pointB = pointPair.1
+      let clusterA = clusterPerPoint[pointA]
+      let clusterB = clusterPerPoint[pointB]
+      if clusterA == -1 && clusterB == -1 {
+        // Neither point is in a cluster yet
+        // Make a new one with 2 points
+        clusterSizes.append(2)
+        clusterPerPoint[pointA] = clusterSizes.count - 1
+        clusterPerPoint[pointB] = clusterSizes.count - 1
+      } else if clusterA == -1 {
+        // Point A is not in a cluster yet, put it in the cluster of point B
+        clusterPerPoint[pointA] = clusterB
+        clusterSizes[clusterB] += 1
+      } else if clusterB == -1 {
+        // Point B is not in a cluster yet, put it in the cluster of point A
+        clusterPerPoint[pointB] = clusterA
+        clusterSizes[clusterA] += 1
+      } else if clusterA != clusterB {
+        // Both A and B are in different clusters, merge them
+        // The one with the higher index gets removed
+        // This is so that all points eventually converge in cluster 0
+        let lower = min(clusterA, clusterB)
+        let higher = max(clusterA, clusterB)
 
-          for i in 0..<clusterPerPoint.count {
-            if clusterPerPoint[i] == higher {
-              clusterPerPoint[i] = lower
-            }
+        for i in 0..<clusterPerPoint.count {
+          if clusterPerPoint[i] == higher {
+            clusterPerPoint[i] = lower
           }
-          clusterSizes[lower] += clusterSizes[higher]
-          // No need for cleanup, since we don't only index 0 matters in the end
-          // clusterSizes[higher] = 0
         }
-
-        if clusterSizes[0] == points.count {
-          break
-        }
+        clusterSizes[lower] += clusterSizes[higher]
+        // No need for cleanup, since we don't only index 0 matters in the end
+        // clusterSizes[higher] = 0
       }
+
       if clusterSizes[0] == points.count {
         break
       }
@@ -177,8 +136,32 @@ struct Solution8: Solution {
     return String(points[lastPointPair.0].x * points[lastPointPair.1].x)
   }
 
+  func getPairsByDistance(points: [Point]) throws -> [(Int, Int)] {
+    // Store the distances between each point and every other point
+    // We are using a dictionary, since we really care about the distance value and want to sort
+    // by it, but after that we also need the points.
+    var distances = [Int: (Int, Int)]()
+    for x in 0..<points.count {
+      let pointX = points[x]
+      for y in (x + 1)..<points.count {
+        let pointY = points[y]
+        let dist = pointX.distance(to: pointY)
+        if distances[dist] != nil {
+          // Technically this is possible in the constraints of the puzzle, but it doesn't happen.
+          // And assuming it won't happen makes the code simpler and faster.
+          // Make it an array if you have to
+          throw SolutionError.distanceCollision
+        }
+        distances[dist] = (x, y)
+      }
+    }
+
+    return distances.sorted(by: { $0.key < $1.key }).map({ $0.value })
+  }
+
   enum SolutionError: Error {
     case noLastPointPair
+    case distanceCollision
   }
 
   struct Point: Equatable {
