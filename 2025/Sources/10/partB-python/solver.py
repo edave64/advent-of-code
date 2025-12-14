a36c8aac9f367db73fa36a0f911547c6b6d37aa3
edave64 <github5815@xcvbnm.org>
2025-12-14 21:14:11 +0100

    Day 10: Slightly improved python

--- 2025/Sources/10/partB-python/solver.py
+++ 2025/Sources/10/partB-python/solver.py
@@ -1,34 +1,30 @@
 import pulp
 
-def solve_system(equations, num_vars):
-    """
-    equations: list of (coeffs, rhs)
-        coeffs = list of length num_vars
-        rhs = integer
-    num_vars: number of variables
-    """
+def solve_system(equations: list[tuple[list[int], int]]) -> list[int]:
+    num_vars = len(equations[0][0])
 
     # Define problem
     prob = pulp.LpProblem("IntegerEquationSystem", pulp.LpMinimize)
 
     # Variables: positive integers
-    x = [pulp.LpVariable(f"x{i}", lowBound=0, cat="Integer")
+    variables = [pulp.LpVariable(f"x{i}", lowBound=0, cat="Integer")
          for i in range(num_vars)]
 
     # Objective: minimize sum of variables
-    prob += pulp.lpSum(x)
+    # This needs to be the first thing added to the problem
+    prob += pulp.lpSum(variables)
 
     # Constraints
-    for coeffs, rhs in equations:
-        prob += pulp.lpSum(coeffs[i] * x[i] for i in range(num_vars)) == rhs
+    for factors, result in equations:
+        prob += pulp.lpSum(factors[i] * variables[i] for i in range(num_vars)) == result
 
-    # Solve
+    # Solve with the integrated solver
     status = prob.solve(pulp.PULP_CBC_CMD(msg=False))
 
     if pulp.LpStatus[status] != "Optimal":
-        return None
+        raise Exception("No solution found")
 
-    return [int(v.value()) for v in x]
+    return [int(v.value()) for v in variables]
 
 # `sum` is taken by a global function
 sum_ = 0
@@ -50,7 +46,7 @@ with open('../../../Data/input10.txt') as f:
         for i in range(len(target)):
             equations.append(([1 if i in button else 0 for button in buttons], target[i]))
         
-        r = solve_system(equations, len(buttons))
+        r = solve_system(equations)
         print(' '.join([str(result) for result in r]))
         ret = sum(r)
 
